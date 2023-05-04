73fe555ef585f52fd38598146ca2810a975d6ff5
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-05-04 18:43:01 +0300

    Needs cleaning, but you can now add  a column

--- admin.php
+++ admin.php
@@ -52,18 +52,83 @@
       </div>
     </div>
     <!-- End Header -->
-    <form class="add-container" method="post" action="add_user.php">
-      <input type="file" name="myfile" />
-      <input class="book-name" name="name" type="text" placeholder="name" />
-      <input class="book-name" name="price" type="number" placeholder="price" />
-      <textarea class="book-disc" name="description" placeholder="description"></textarea>
+
+    <?php 
+      // if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // //   // to download the uploaded image and put it inside the Downloads folder
+        // $img = $_FILES['book-img'];
+        // $img_name = $img['name'];
+        // $img_location = $img['tmp_name'];
+        // $target_dir = "Downloads/";
+        // $target_file = $target_dir . basename($img_name);
+        // copy($img_location, $target_file);
+      // }
+      
+      //to connect to database server
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "book-data";
+
+        $conn = mysqli_connect($host, $user, $pass, $db);
+
+        $r = mysqli_query($conn, "SELECT * FROM booktable");
+      // if(isset($_POST['add-btn'])) {
+        $name = "";
+        $price = "";
+        $disc = "";
+        $book_cover = "";
+
+        if( isset($_POST['book-name'])){
+          $name = $_POST['book-name'];
+        }
+
+        if( isset($_POST['book-price'])){
+          $price = $_POST['book-price'];
+        }
+
+        if( isset($_POST['book-description'])){
+          $disc = $_POST['book-description'];
+        }
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+          $img = $_FILES['book-img'];
+          $img_name = $img['name'];
+          $img_location = $img['tmp_name'];
+          $target_dir = "Downloads/";
+          $target_file = $target_dir . basename($img_name);
+          copy($img_location, $target_file);
+          $book_cover = $img_name;
+        }
+        $sql = "";
+        if( isset($_POST['add-btn'])) {
+          $sql = "insert into booktable (Name, Price, Description, Image) values('$name', $price, '$disc', '$book_cover')";
+          mysqli_query($conn, $sql);
+        }
+
+        // //to insert query into database
+        // $insert = "insert into booktable (Name, Price, Description, Image) values('The KIM', 50, 'Lorem Ipsum', 'img.png')";
+        // $query = mysqli_query($conn, $insert);
+
+        // if($query) {
+        //   echo "OK";
+        // }
+      //}
+
+    ?>
+
+    <form class="add-container" method="post" enctype="multipart/form-data">
+      <input type="file" name="book-img" />
+      <input class="book-name" id= "bname" name="book-name" type="text" placeholder="name" />
+      <input class="book-name" id= "bprice" name="book-price" type="number" placeholder="price" />
+      <textarea class="book-disc" id= "bdes" name="book-description" placeholder="description"></textarea>
       <div class="btn-container">
-      <input class="add-btn" type="submit" name="add" value="Add">
-      <input class="edit-btn" type="submit" name="edit" value="Edit">
-      <input class="delete-btn" type="submit" name="delete" value="Delete">
+      <input class="add-btn" type="submit" name="add-btn" value="Add">
+      <input class="edit-btn" type="submit" name="edit-btn" value="Edit">
+      <input class="delete-btn" type="submit" name="delete-btn" value="Delete">
       </div>
     </form>
-    <table>
+    <table id="booktable">
         <thead>
           <tr>
             <th style="width:15%">Name</th>
@@ -73,74 +138,34 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
-          <tr>
-            <td data-column="Name">Lorem</td>
-            <td data-column="Price">100</td>
-            <td data-column="Description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione iste alias reprehenderit fuga dolore, et distinctio, rem in hic, nobis voluptate omnis. Voluptates neque magnam quis placeat harum, reprehenderit ullam!</td>
-            <td data-column="Image">Lorem.png</td>
-          </tr>
+        <?php
+          while ( $row = mysqli_fetch_array($r)) {
+            echo "<tr>";
+              "<td>" . $row['ID'] . "</td>";
+              echo "<td>" . $row['Name'] . "</td>";
+              echo "<td>" . $row['Price'] . "</td>";
+              echo "<td>" . $row['Description'] . "</td>";
+              echo "<td>" . $row['Image'] . "</td>";
+            echo "</tr>";
+          }
+          ?>
         </tbody>
       </table>
+
+      <!-- javascript code to get the data in the tables and add it in the form when clicked-->
+      <script>
+        var tbl = document.getElementById("booktable");
+
+        for (var x = 1; x < tbl.rows.length ; x++){
+          tbl.rows[x].onclick = function(){
+            document.getElementById("bname").value = this.cells[0].innerHTML
+            document.getElementById("bprice").value = this.cells[1].innerHTML
+            document.getElementById("bdes").value = this.cells[2].innerHTML
+          }
+        }
+
+      </script>
+
       <!-- start footer -->
       <div class="footer"> 
         <div class="contanier_footer">
